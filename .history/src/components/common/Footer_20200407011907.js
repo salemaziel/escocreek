6b1dd51e315fb9e7b9c70b8fd2f03490771816a6
salemaziel <mymainemail0501@gmail.com>
2020-04-07 11:28:56 -0700

    asgagfv

--- .history/src/components/common/Footer_20200407011907.js
+++ .history/src/components/common/Footer_20200407011907.js
@@ -7,6 +7,9 @@ const Footer = () => (
     <div className="inner">
       <ul className="menu">
         <li>&copy; Untitled. All rights reserved.</li>
+        <li>
+          Design: <a href="http://html5up.net">HTML5 UP</a>
+        </li>
       </ul>
     </div>
   </footer>
